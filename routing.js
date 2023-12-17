4e387d6f448dbc48d910e3ae82c175dbd855c2a3
jatinscodingproject <152329190+jatinscodingproject@users.noreply.github.com>
2023-12-18 01:44:14 +0530

    # routing request

--- routing.js
+++ routing.js
@@ -1,7 +1,22 @@
 const http = require('http')
+const fs = require('fs')
 
 const server = http.createServer((req,res) => {
     const url = req.url
+    const method = req.method
+    if (url === "/"){
+        res.write('<html>')
+        res.write('<head><title> My First Page</title></head>')
+        res.write('<body><form action="/message" method="POST"><input type ="text"><button type="submit">send</button></form></body>')
+        res.write('</html>')
+        return res.end()
+    }
+    if (url === "/message" && method === "POST"){
+        fs.writeFileSync('message.txt','DUMMY')
+        res.statusCode = 302;
+        res.setHeader('Location','/')
+        return res.end()
+    }
     res.setHeader('Content-Type','text/html')
     res.write('<html>')
     res.write('<head><title> My First Page</title></head>')
