86e18b0264925ec2c4b97b6a6d62c3a07f9dd40c
jatinscodingproject <152329190+jatinscodingproject@users.noreply.github.com>
2023-12-18 01:16:12 +0530

    # using url method to server various user requerst

--- first_server.js
+++ first_server.js
@@ -1,13 +1,27 @@
 const http = require('http')
-
+const url = require('url')
 const server = http.createServer((req,res) => {
-    console.log(req.url , req.method , req.headers)
-    res.setHeader('Content-Type','text/html')
-    res.write('<html>')
-    res.write('<head><title> My first page </title></head>')
-    res.write('<h1> JATIN DIXIT </h1>')
-    res.write('</html>')
-    res.end()
+    // console.log(req.url , req.method , req.headers)
+    if (url === '/home'){
+        res.setHeader('Content-Type','text/html')
+        res.write('<h1> Welcome home </h1>')
+        res.end()
+    }
+    else if (url === '/about'){
+        res.setHeader('Content-Type','text/html')
+        res.write('<h1> Welcome To about us Page </h1>')
+        res.end()
+    }
+    else if (url === '/node'){
+        res.setHeader('Content-Type','text/html')
+        res.write('<h1>Welcome to Node js server</h1>')
+        res.end()
+    }
+    else {
+        res.setHeader('Content-Type','text/html')
+        res.write('<h1>Page Not Found</h1>')
+        res.end()
+    }
 })
 
 server.listen(4000)
